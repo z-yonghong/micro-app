2c0c41c20e677793b4212561046876ffd0824b12
fightingfightingfighting <3230744579@qq.com>
2024-08-19 18:26:59 +0800

    添加react子项目

---
+++ sub-react/config-overrides.js
@@ -0,0 +1,12 @@
+const { name } = require('./package');
+
+module.exports = {
+  webpack: (config) => {
+    config.output.library = `${name}-[name]`;
+    config.output.libraryTarget = 'umd';
+    config.output.chunkLoadingGlobal = `webpackJsonp_${name}`;
+    config.output.globalObject = 'window';
+
+    return config;
+  },
+};
